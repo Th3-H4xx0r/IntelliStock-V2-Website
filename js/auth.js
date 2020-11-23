4838d586f9c333a9d4e01505e7ea9d025ad23e75
Pranav Krishna <krishnatechpranav@gmail.com>
2020-11-23 10:30:12 -0800

    sadf

--- js/auth.js
+++ js/auth.js
@@ -2,9 +2,40 @@ function login(){
     document.getElementById('login-btn').innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`
     document.getElementById('login-btn').disabled = true;
 
+    var error = document.getElementById('error');
+
     var email = document.getElementById('email').value
     var password = document.getElementById('password').value
 
-    console.log(email)
-    console.log(password)
+    if(email != null && email != '' && password != null && password != ''){
+
+    var authValid = true;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorMessage);
+
+        error.innerHTML = errorMessage;
+
+        authValid = false;
+        // ...
+    }).then(() => {
+        console.log(authValid)
+        if(authValid == true){
+            error.innerHTML = ''
+        } else {
+            document.getElementById('login-btn').innerHTML = `Login`
+            document.getElementById('login-btn').disabled = false;
+        }
+    })
+
+
+    } else {
+        document.getElementById('login-btn').innerHTML = `Login`
+        document.getElementById('login-btn').disabled = false;
+        error.innerHTML = "You cannot leave any fields blank"
+    }
 }
