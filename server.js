7f118ab4b715548a9bf0b50e713bcf4ea4501144
Pranav Krishna <krishnatechpranav@gmail.com>
2020-11-26 11:07:40 -0800

    adsf

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
   })
   
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
     next();
   });
 
