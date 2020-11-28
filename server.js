314b1ebd4cfc25fbc3488f77cf411a86ca4b68c6
Pranav Krishna <krishnatechpranav@gmail.com>
2020-11-28 13:16:27 -0800

    daf

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ router.get('/stocks',function(req,res){
 router.get('/stock',function(req,res){
   //var tickerCode = req.query.ticker
   //res.render(path.join(__dirname+'/stock-info.ejs'), {ticker: "TSLA"})
-  res.sendFile(path.join(__dirname+'/stock-info.html'));
+  res.sendFile(path.join(__dirname+'/stock-info.ejs'));
 
 });
 
